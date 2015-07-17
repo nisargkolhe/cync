2d3a9a503b20aa9f2bedb799692327c5120bfb30
nisargkolhe <nisargkolhe@gmail.com>
2015-07-17 19:13:13 +0530

    ->Merged to master

--- app/src/main/java/com/clubcypher/cync/MainActivity.java
+++ app/src/main/java/com/clubcypher/cync/MainActivity.java
@@ -304,7 +304,8 @@ public class MainActivity extends AppCompatActivity {
         //handle the back press :D close the drawer first and if the drawer is closed close the activity
         if (result != null && result.isDrawerOpen()) {
             result.closeDrawer();
-        } else {
+        }
+        else {
             super.onBackPressed();
         }
     }
